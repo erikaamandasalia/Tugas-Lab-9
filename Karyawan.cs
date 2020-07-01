7c1976b91aedca5ea1c9258fa0ec0c4f679eb275
erikaamandasalia <erika.rumopa@students.amikom.ac.id>
2020-07-01 21:47:08 +0700

    Inheritance,Polymorphsim,Abstraction & Collection Bagian #2

---
+++ Karyawan.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tugas_lab_9
+{
+    public abstract class Karyawan
+    {
+        public string NIK;
+        public string Nama;
+        public abstract double Gaji();
+    }
+}
